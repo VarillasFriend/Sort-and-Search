3648338b2f8c64ad92845820dd7e0c4d5c6e1bb0
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-12-22 14:08:50 -0300

    Improve bubble sort

--- sort/bubble_sort.rb
+++ sort/bubble_sort.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
-def bubble_sort(a)
-    a.length.times do
-        a.each_with_index do |element, index|
-            next if index == (a.length - 1)
-            next unless element > a[index + 1]
+def bubble_sort(array)
+    swaps = 1
 
-            element_2 = a[index + 1]
-            a[index + 1] = element
-            a[index] = element_2
+    until swaps.zero?
+        swaps = 0
+
+        (array.length - 1).times do |index|
+            next if array[index] <= array[index + 1]
+
+            element2 = array[index + 1]
+            array[index + 1] = array[index]
+            array[index] = element2
+
+            swaps += 1
         end
     end
 
-    a
+    array
 end
