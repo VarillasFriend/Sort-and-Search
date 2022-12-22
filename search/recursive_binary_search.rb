2e1ff72fd7be774e177570521aa4e057633a0493
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-12-22 14:43:39 -0300

    Improve recursive binary search

--- search/recursive_binary_search.rb
+++ search/recursive_binary_search.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 def recursive_binary_search(a, n)
-    number = nil
+    return a[a.length / 2] if a.length == 1 || a[a.length / 2] == n
 
-    if (a.length == 1) || (a[a.length / 2] == n)
-        a[a.length / 2]
-
-    else
-        a = a[a.length / 2] > n ? a[0...a.length / 2] : a[a.length / 2...a.length]
-        recursive_binary_search(a, n)
-    end
+    recursive_binary_search(a[a.length / 2] > n ? a[0...a.length / 2] : a[a.length / 2...a.length], n)
 end
